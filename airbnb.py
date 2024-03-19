8eeedb84e999b8cda5c964d14f05833018c4d797
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2024-03-19 08:41:35 +0530

    connected with mongodb database

--- airbnb.py
+++ airbnb.py
@@ -1,4 +1,26 @@
-import streamlit as st
+#import streamlit as st
+from pymongo import MongoClient
+from client import mongo
 
-st.set_page_config(layout="wide")
+client = MongoClient(mongo)
+
+db = client["airbnb"]
+
+collection = db["listings"]
+
+query = {
+    "accommodates": {"$gte": 6},
+    "price": {"$lte": 100}
+}
+
+results = collection.find(query)
+
+for listing in results:
+    print("Name:", listing["name"])
+    print("Listing URL:", listing["listing_url"])
+    print("Price:", listing["price"])
+    print("Accommodates:", listing["accommodates"])
+    print("--------------")
+
+#st.set_page_config(layout="wide")
 

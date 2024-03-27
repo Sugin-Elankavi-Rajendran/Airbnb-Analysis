726638c8fb60f6c6fcf1b67093158aa6fa017193
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2024-03-27 09:37:42 +0530

    downloaded the airbnb database

--- airbnb.py
+++ airbnb.py
@@ -2,15 +2,31 @@ from pymongo import MongoClient
 from client import mongo
 import pandas as pd
 import streamlit as st
-import folium
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-from streamlit_folium import folium_static
+
+############
+
+client = MongoClient(mongo)
+db = client["sample_airbnb"]
+collection = db["listingsAndReviews"]
+
+cursor = collection.find({})
+df = pd.DataFrame(list(cursor))
+
+csv_file_path = "listingsAndReviews.csv"
+df.to_csv(csv_file_path, index=False)
+
+print("CSV file has been saved successfully.")
 
 ############
 
 st.set_page_config(layout="wide")
 st.title('Airbnb Analysis')
 
+uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
+
+if uploaded_file is not None:
+        st.write("File uploaded successfully!")
+        df = pd.read_csv(uploaded_file)
+        st.write("Data Preview:")
+        st.write(df.head())
 

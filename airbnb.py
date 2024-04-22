234ba2862d4057ce118487a411f0f8aaf4b33d3f
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2024-04-22 18:21:36 +0530

    cleaned the data

--- airbnb.py
+++ airbnb.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import ast
+import datetime
 
 ############
 
@@ -23,8 +24,9 @@ if uploaded_file is not None:
                  'weekly_price','monthly_price','cleaning_fee','images','listing_url'], axis=1, inplace=True)
         df.dropna(subset=['name'], inplace=True)
         df.dropna(subset=['first_review'], inplace=True)
-        df.dropna(subset=['reviews_per_month'], inplace=True)
-        df.dropna(subset=['security_deposit'], inplace=True)
+        
+        df['reviews_per_month'].fillna(df['reviews_per_month'].mean(), inplace=True)
+        df['security_deposit'].fillna(df['security_deposit'].mean(), inplace=True)
         df['bedrooms'].fillna(df['bedrooms'].mean(), inplace=True)
         df['beds'].fillna(df['beds'].mean(), inplace=True)
         df['bathrooms'].fillna(df['bathrooms'].mean(), inplace=True)
@@ -38,6 +40,48 @@ if uploaded_file is not None:
         df['host_total_listings_count'] = df['host'].apply(lambda x: x.get('host_total_listings_count'))
 
         df.drop(columns=['host'], inplace=True)
+
+        df['reviews'] = df['reviews'].str.strip('[]').str.split(',')
+        for key in ['reviewer_name', 'comments']:
+                df[key] = df['reviews'].apply(lambda reviews_list: [review.split(':')[1].strip().strip("'\"") if len(review.split(':')) > 1 else None for review in reviews_list if key in review])
+                
+        df.drop(columns=['reviews'], inplace=True)
+
+        df['review_scores'] = df['review_scores'].apply(ast.literal_eval)
+        df['review_scores_accuracy'] = df['review_scores'].apply(lambda x: x.get('review_scores_accuracy'))
+        df['review_scores_cleanliness'] = df['review_scores'].apply(lambda x: x.get('review_scores_cleanliness'))
+        df['review_scores_checkin'] = df['review_scores'].apply(lambda x: x.get('review_scores_checkin'))
+        df['review_scores_communication'] = df['review_scores'].apply(lambda x: x.get('review_scores_communication'))
+        df['review_scores_location'] = df['review_scores'].apply(lambda x: x.get('review_scores_location'))
+        df['review_scores_value'] = df['review_scores'].apply(lambda x: x.get('review_scores_value'))
+        df['review_scores_rating'] = df['review_scores'].apply(lambda x: x.get('review_scores_rating'))
+
+        df.drop(columns=['review_scores'], inplace=True)
+        df.dropna(subset=['review_scores_value'], inplace=True)
+        df.dropna(subset=['review_scores_accuracy'], inplace=True)
+        df.dropna(subset=['review_scores_cleanliness'], inplace=True)
+        df.dropna(subset=['review_scores_checkin'], inplace=True)
+        df.dropna(subset=['review_scores_rating'], inplace=True)
+
+        df['availability'] = df['availability'].apply(ast.literal_eval)
+        df['availability_30'] = df['availability'].apply(lambda x: x.get('availability_30'))
+        df['availability_60'] = df['availability'].apply(lambda x: x.get('availability_60'))
+        df['availability_90'] = df['availability'].apply(lambda x: x.get('availability_90'))
+        df['availability_365'] = df['availability'].apply(lambda x: x.get('availability_365'))
+
+        df.drop(columns=['availability'], inplace=True)
+
+        df['address'] = df['address'].apply(ast.literal_eval)
+        df['street'] = df['address'].apply(lambda x: x.get('street'))
+        df['suburb'] = df['address'].apply(lambda x: x.get('suburb'))
+        df['government_area'] = df['address'].apply(lambda x: x.get('government_area'))
+        df['market'] = df['address'].apply(lambda x: x.get('market'))
+        df['country'] = df['address'].apply(lambda x: x.get('country'))
+        df['country_code'] = df['address'].apply(lambda x: x.get('country_code'))
+        df['latitude'] = df['address'].apply(lambda x: x.get('location', {}).get('coordinates', [])[0] if x.get('location') else None)
+        df['longitude'] = df['address'].apply(lambda x: x.get('location', {}).get('coordinates', [])[1] if x.get('location') else None)
+
+        df.drop(columns=['address'], inplace=True)
 
         missing_values = df.isnull().sum()
         st.write("Missing values:\n", missing_values)
